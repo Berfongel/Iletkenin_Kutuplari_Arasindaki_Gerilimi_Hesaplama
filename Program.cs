fcff7dba3c5a1891c9376ef2cf8ab38b5edd3898
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 15:18:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int akim,direnc,gerilim;
+Console.WriteLine("Akım(I) değerini giriniz : ");
+akim = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Direnç(R) değerini giriniz : ");
+direnc = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Gerilim(V) değeri : "   + (direnc * akim));
+gerilim = Convert.ToInt32(Console.ReadLine());
